gabrielelana/vim-markdown

commit 60a0e2dfe9174bbded7cffd18fdbca6c654c7e08

Fix #48

autoload/markdown.vim
@@ -87,9 +87,9 @@ function! markdown#EditBlock() range abort
     echo 'Sorry, you cannot edit a code block inside a temporary buffer'
     return
   endif
-  " Github fenced code blocks like ```ruby
+  " Github fenced code blocks like ```ruby or ```{ruby, <WHATEVER>}
   let code_block = s:LocateFencedCodeBlock(a:firstline,
-    \ '^\s*`\{3,}\(\w\+\)\%(\s.*$\|$\)',
+    \ '^\s*`\{3,}\%({\(\w\+\),[^}]\+}\|\(\w\+\)\)\%(\s.*$\|$\)',
     \ '^\s*`\{3,}\s*$'
     \ )
   if code_block['from'] == 0 || code_block['to'] == 0

commit 21fbfe1bb1a182f88146f76a426b17a976be96b2

Fix regression introduced with 60a0e2d

autoload/markdown.vim
@@ -87,11 +87,18 @@ function! markdown#EditBlock() range abort
     echo 'Sorry, you cannot edit a code block inside a temporary buffer'
     return
   endif
-  " Github fenced code blocks like ```ruby or ```{ruby, <WHATEVER>}
+  " Github fenced code blocks like ```ruby
   let code_block = s:LocateFencedCodeBlock(a:firstline,
-    \ '^\s*`\{3,}\%({\(\w\+\),[^}]\+}\|\(\w\+\)\)\%(\s.*$\|$\)',
+    \ '^\s*`\{3,}\(\w\+\)\%(\s.*$\|$\)',
     \ '^\s*`\{3,}\s*$'
     \ )
+  if code_block['from'] == 0 || code_block['to'] == 0
+    " Github fenced code blocks with metadata like ```{ruby, <WHATEVER>}
+    let code_block = s:LocateFencedCodeBlock(a:firstline,
+      \ '^\s*`\{3,}{\(\w\+\),[^}]\+}\%(\s.*$\|$\)',
+      \ '^\s*`\{3,}\s*$'
+      \ )
+  endif
   if code_block['from'] == 0 || code_block['to'] == 0
     " Github fenced code blocks alternate style like ~~~ruby
     let code_block = s:LocateFencedCodeBlock(a:firstline,